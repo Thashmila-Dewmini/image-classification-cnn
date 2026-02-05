11d8740ffef0e82a3dd7809ae9733998a1ff0f6c
Thashmila Dewmini <thashmiladewminiwork@gmail.com>
2026-02-05 08:40:18 +0530

    Updated model.py file

--- src/model.py
+++ src/model.py
@@ -2,24 +2,27 @@ import keras
 from keras import layers, models # models - tools to define model structures (Sequential, Model)
 
 def build_model():
-    data_augmentation  = keras.Sequential([
-        layers.RandomRotation(0.04),
-        layers.RandomTranslation(0.1, 0.1),
-        layers.RandomFlip('horizontal')
+
+    # Data augmentation pipeline : preprocessing layers
+    # this will slightly changes images every time they pass through
+    data_augmentation  = keras.Sequential([   # create mini NN, no trainable weights and only job is randomly transform images
+        layers.RandomRotation(0.04), # randomly rotate left or right and rotation range is ±4% of 360° : this will help to recognize objects at slightly different angles
+        layers.RandomTranslation(0.1, 0.1), # shifts the image left/right by up to 10% of width and up/down by up to 10% of height
+        layers.RandomFlip('horizontal') # randomly flips images left <-> right
     ])
 
     # Build CNN
     # Sequential model : layers are stacked one after another
     model = models.Sequential([
         layers.Input(shape=(32,32,3)), # input images with 32x32 pixel and 3 color channels
-        data_augmentation ,
+        data_augmentation,
 
         layers.Conv2D(32, (3,3), activation='relu', padding='same'),  # 32 filters and each filter size is 3x3
         layers.BatchNormalization(),
         layers.MaxPooling2D(), # reduces image size by taking maximum value in each 2x2 area
 
         layers.Conv2D(64, (3,3), activation='relu', padding='same'),
-        layers.BatchNormalization(),
+        layers.BatchNormalization(), # normailze layer outputs from previous layer
         layers.MaxPooling2D(),
 
         layers.Conv2D(128, (3,3), activation='relu', padding='same'),
